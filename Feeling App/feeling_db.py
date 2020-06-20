715a6d623747441daa0e854c4ba805590fcd9104
NickToz <48648347+NickToz@users.noreply.github.com>
2020-06-20 14:17:09 +0100

    Link to postgresql

--- Feeling App/feeling_db.py
+++ Feeling App/feeling_db.py
@@ -7,3 +7,4 @@ c.execute('''CREATE TABLE feeling
              (date TEXT, feeling_1 INTEGER, feeling_2 INTEGER, q1 INTEGER, q2 INTEGER, q3 INTEGER, q4 INTEGER, q5 INTEGER)''')
 
 c.close()
+
--- Feeling App/feeling_prog.py
+++ Feeling App/feeling_prog.py
@@ -2,26 +2,51 @@
 import sqlite3
 import time
 import datetime
+import psycopg2 as p
+
+# homwork look for psycopg2 - do what it in quey editor but from python
+
+
 
 def main():
-    create_table()
+    #connect()
+    #create_table()
     insert_feeling_1()
+    show_rows()
+    
   
+"""USE THIS IF USING SQLite3
+
 conn = sqlite3.connect('feeling.db')
 c = conn.cursor()
 
 def create_table():
     c.execute('''CREATE TABLE IF NOT EXISTS feeling
-             (date TEXT, feeling_1 INTEGER, feeling_2 INTEGER, q1 INTEGER, q2 INTEGER, q3 INTEGER, q4 INTEGER, q5 INTEGER)''')
+             (date TEXT, feeling_0 INTEGER, feeling_1 INTEGER, q1 INTEGER, q2 INTEGER, q3 INTEGER, q4 INTEGER, q5 INTEGER)''')
+
+"""
+
+# FOR CONNECTING WITH POSTGRES DB
+
+# postgres host  ec2-54-75-246-118.eu-west-1.compute.amazonaws.com
+
+#def connect():
+conn = p.connect("dbname=d45ir25h8c6ka user=jkqlddsqykkyqw host=ec2-54-75-246-118.eu-west-1.compute.amazonaws.com password=7c7329ca43ab68a68f288e770789e4ce0ac4a35b1442367c05ade823f087b5a0")
+c = conn.cursor()
+
+def show_rows():
+    c.execute("select * from feeling")
+    rows = c.fetchall()
+    for r in rows:
+        print(r)
+
 
 def insert_feeling_1():
-    feeling_1 = int(input("On a scale of 1 to 10, 1 being the worst, 10 being the best, how are you feeling today? "))
-    while feeling_1 < 1 or feeling_1 > 10:
+    f1 = int(input("On a scale of 1 to 10, 1 being the worst, 10 being the best, how are you feeling today? "))
+    while f1 < 1 or f1 > 10:
       int(input('not valid. Please enter a number between 1 and 10:'))
-    c.execute("INSERT INTO feeling (feeling_1) VALUES (?)",
-    (feeling_1))
+    c.execute("INSERT INTO feeling (feeling_1) values ({})".format(f1))
     conn.commit()
-
 
 
 
