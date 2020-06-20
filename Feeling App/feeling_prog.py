346c17651b797ea23fec6b350a7932501da5211c
NickToz <48648347+NickToz@users.noreply.github.com>
2020-06-20 15:28:13 +0100

    input functions

--- Feeling App/feeling_prog.py
+++ Feeling App/feeling_prog.py
@@ -11,7 +11,7 @@ import psycopg2 as p
 def main():
     #connect()
     #create_table()
-    insert_feeling_1()
+    ask_questions()
     show_rows()
     
   
@@ -40,15 +40,41 @@ def show_rows():
     for r in rows:
         print(r)
 
+def feeling_input():
+    f = int(input("On a scale of 1 to 10, 1 being the worst, 10 being the best, how are you feeling today? "))
+    while f < 1 or f > 10:
+        f = int(input('not valid. Please enter a number between 1 and 10:'))
+    return f
 
-def insert_feeling_1():
-    f1 = int(input("On a scale of 1 to 10, 1 being the worst, 10 being the best, how are you feeling today? "))
-    while f1 < 1 or f1 > 10:
-      int(input('not valid. Please enter a number between 1 and 10:'))
-    c.execute("INSERT INTO feeling (feeling_1) values ({})".format(f1))
+
+def ask_questions():
+    f0 = feeling_input()
+    f1 = feeling_input()
+    q1 = q_input("Have you exercised?")
+    q2 = q_input("Have you cooked a healthy meal?")
+    q3 = q_input("Have you had a meaningful conversation?")
+    q4 = q_input("Have you smoked?")
+    q5 = q_input("Have you drank alcohol?")
+    commit_to_db(datetime.datetime.utcnow(), f0, f1, q1, q2, q3, q4, q5)
+
+def q_input(what_to_ask):
+    q = input(what_to_ask)
+    # while q not in ("y", "n"):
+    #    q = input("please enter y or n")
+    if q == "y":
+        return True
+    else:
+        return False
+
+
+def commit_to_db(timedate,f0,f1,q1,q2,q3,q4,q5):
+    c.execute("INSERT INTO feeling (datetime, feeling_0, feeling_1, q1, q2, q3, q4, q5) values ('{}',{},{},{},{},{},{},{})".format(timedate,f0,f1,q1,q2,q3,q4,q5))
     conn.commit()
 
-
+# one function for feeling scales, one for booleans, one for inserting into database.
+# different prompts for feeling inputs
+# unique identifier
+# tell user whether it's a yes or no question - find smart way - string concatonation.
 
 
 if __name__=='__main__':
